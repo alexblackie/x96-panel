681d50ce27403071f5bbc397d6fcb86c14cbe9e2
Alex Blackie <alex@alexblackie.com>
2010-08-12 05:24:05 +0000

    Added getInfo() function.
    git-svn-id: svn://localhost/tmp/x96/trunk@32 d6135677-3d94-4058-949d-3129dc27eef7

--- system/functions.php
+++ system/functions.php
@@ -2,10 +2,20 @@
 	// Functions file. This file has all of the functions used by templates
 	// and other stuff around the application.
 	
+	function getInfo($for) {
+		// Queries the database for the value of $for.
+	
+		$sql = mysql_query("SELECT * from " . APP_TABLEPREFIX . "options WHERE `option` = '$for'") or die("<h1>An incorrect parameter was set for <code>getInfo()</code>.</h1><p>Please check your code for any errors.</p>");
+		while($row = mysql_fetch_array($sql)) {
+			return $row[1];
+		}
+	
+	}
+	
 	function load_theme() {
 		// Probably the most important function in the entire program. This
 		// function loads the current theme.
 	
-		require(APP_BASEDIR . "templates/" . APP_THEME . "/template.php") or die("<h1>Loading the Template Failed.</h1><p>X96 CMS Could not load the template. This could mean that the template file doesn't exist.</p>");
+		require(APP_BASEDIR . "templates/" . getInfo('current_theme') . "/template.php") or die("<h1>Loading the Template Failed.</h1><p>X96 CMS Could not load the template. This could mean that the template file doesn't exist.</p>");
 			
 	}
