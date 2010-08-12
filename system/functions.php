7400f0ffe6908d375051ee17467f085e22eabf2d
Alex Blackie <alex@alexblackie.com>
2010-08-12 04:57:44 +0000

    Added function load_theme()
    git-svn-id: svn://localhost/tmp/x96/trunk@27 d6135677-3d94-4058-949d-3129dc27eef7

--- system/functions.php
+++ system/functions.php
@@ -2,4 +2,10 @@
 	// Functions file. This file has all of the functions used by templates
 	// and other stuff around the application.
 	
+	function load_theme() {
+		// Probably the most important function in the entire program. This
+		// function loads the current theme.
 	
+		require APP_BASEDIR . "templates/" . APP_THEME . "/template.php" or die("<h1>Loading the Template Failed.</h1><p>X96 CMS Could not load the template. This could mean that the template file doesn't exist.</p>");
+			
+	}
