ba160877a2e881306cfb695c27751d8b49d97a38
Alex Blackie <alex@alexblackie.com>
2010-08-12 19:23:33 +0000

    First draft of the session protect method... Probably insecure, but will be tested.
    git-svn-id: svn://localhost/tmp/x96/trunk@59 d6135677-3d94-4058-949d-3129dc27eef7

--- system/protect.php
+++ system/protect.php
@@ -1,2 +1,15 @@
 <?php
+
+	if(!isset($_SESSION['login'])){
+		// Login session variable not set? Then set it to "not logged in".
+		$_SESSION['login'] = true;
+	}
 	
+	if($_SESSION['login'] === false) {
+		// Not logged in? Redirect to login form
+		header("Location: login.php");
+	}
+	elseif($_SESSION['login'] === true) {
+		// Logged in? Great - redirect to index page.
+		header("Location: manage.php");
+	}
