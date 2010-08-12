8fd130619da06611b0e551503f48351c512d6085
Alex Blackie <alex@alexblackie.com>
2010-08-12 02:55:43 +0000

    Added database connection info defines.
    git-svn-id: svn://localhost/tmp/x96/trunk@22 d6135677-3d94-4058-949d-3129dc27eef7

--- global_config.php
+++ global_config.php
@@ -1,3 +1,19 @@
 <?php
 	// The global config file. This is where all the defines are as well as a
 	// bunch of other stuff like database connection and functions.
+	
+	// Set the database host
+	// Don't change unless you KNOW that it's not localhost
+	define("APP_DB_HOST", "localhost");
+	
+	// Set the database username
+	// The username to connect to the database.
+	define("APP_DB_USER", "x96cms");
+	
+	// Set the database user password
+	// The password for the user set above.
+	define("APP_DB_PASS", "password");
+	
+	// Set the database name
+	// The name of the database.
+	define("APP_DB_NAME", "x96cms");
