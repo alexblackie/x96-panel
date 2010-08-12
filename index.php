00662998a25d306666bad5d8bf9367b6d129f392
Alex Blackie <alex@alexblackie.com>
2010-08-12 01:24:01 +0000

    Put in opening comment, added require_once for global config and reference to load_theme().
    git-svn-id: svn://localhost/tmp/x96/trunk@17 d6135677-3d94-4058-949d-3129dc27eef7

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+	// This is the frontend to the X96 CMS Application. That means it is
+	// responsible for loading the theme and configuration options.
+	
+	// Load global config file:
+	require_once "global_config.php";
+	
+	// Start loading theme:
+	load_theme();
