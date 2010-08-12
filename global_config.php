aa10f5d12fc0e355ca865bbd0e6682e9fa95c247
Alex Blackie <alex@alexblackie.com>
2010-08-12 05:11:08 +0000

    Linking global_config to functions.php; Modified load_theme to require template file
    git-svn-id: svn://localhost/tmp/x96/trunk@29 d6135677-3d94-4058-949d-3129dc27eef7

--- global_config.php
+++ global_config.php
@@ -29,3 +29,6 @@
 	
 	// Now to select the database...
 	mysql_select_db(APP_DB_NAME);
+	
+	// Require that we have the functions so we can actually do stuff with the app.
+	require APP_BASEDIR . "system/functions.php";
